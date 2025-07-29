823267bd9edf789e328f0c37ae73df451e02ddb9
Luis Vinicius <viniborges@yahoo.com.br>
2025-07-29 16:25:27 -0300

    Resolução dos exercicios iniciais

--- src/main/java/br/com/dio/desafioreactor/Question1.java
+++ src/main/java/br/com/dio/desafioreactor/Question1.java
@@ -9,6 +9,9 @@ public class Question1 {
     /*
     Recebe uma lista de longs, incrementa 1 nos valores e retorna um flux dos resultados
      */
-    public Flux<Long> inc(final List<Long> numbers){}
+    public Flux<Long> inc(final List<Long> numbers){
+        return Flux.fromIterable(numbers)
+                .map(n -> n + 1);
+    }
 
 }
--- src/main/java/br/com/dio/desafioreactor/Question3.java
+++ src/main/java/br/com/dio/desafioreactor/Question3.java
@@ -8,6 +8,11 @@ public class Question3 {
     Verifica se o usuário passado é valido, caso seja retorna void, caso contrário deve disparar uma exception
     (para esse desafio vamos considerar que o usário é valido quando ele tem uma senha com mais de 8 caractéres)
      */
-    public Mono<Void> userIsValid(final User user){}
+    public Mono<Void> userIsValid(final User user){
+        return Mono.fromRunnable(() -> {
+            if (user.password().length() <= 8)
+                throw new IllegalArgumentException("Usuário inválido");
+        });
+    }
 
 }
